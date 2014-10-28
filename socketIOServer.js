724d8e894d2fdd0b5c77087215983af8692d64ef
galicians <galicians@gmail.com>
2014-10-28 18:59:58 +0000

    adding clientWebSocket.js

---
+++ socketIOServer.js
@@ -0,0 +1,10 @@
+var http = require('http'), io = require('socket.io');
+
+var server = http.createServer(function(req, res){ 
+
+	// Send HTML headers and message
+	res.writeHead(200,{ 'Content-Type': 'text/html' }); 
+	res.end('<h1>Hello Socket Lover!</h1>');
+});
+server.listen(8080);
+
