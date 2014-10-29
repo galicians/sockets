3ad0d554cfa884a51700d109d8154d40b7a55fa7
galicians <galicians@gmail.com>
2014-10-29 00:01:29 +0000

    express server added, using request.params

---
+++ serverExpress.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var host = "127.0.0.1"
+var port = 1337
+
+
+var app = express()
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", function(request, response) {
+	response.send("Hello")
+})
+
+app.get("/hello/:text", function(request, response) {
+	response.send("Hello " + request.params.text)
+	console.log(request.params)
+})
+
+app.get("/user/:id", function(request, response) {
+	var user = users[request.params.id]
+	if (user) {
+		response.send('user found')
+	} else {
+		response.send("Sorry we haven't found that user")
+	}
+})
+
+app.listen(port, host)
